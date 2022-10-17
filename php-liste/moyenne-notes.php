620632402b8132d5bb846cc13a9bdcd7b79bc44a
Alexis Bassignot <a.bassignot@orange.fr>
2022-10-17 15:26:00 +0200

    17 10 2022 15:25

--- php-liste/moyenne-notes.php
+++ php-liste/moyenne-notes.php
@@ -1,4 +1,38 @@
 <?php
-$listeNotes = [10, 12.5, 14, 7, 15, 9.5, 8];
 
-foreach ($listeNotes as $note) {}
+function calculMention($moyenne) {
+    if ($moyenne >=0 && $moyenne < 10) {
+        $phrase = "Vous n'avez pas votre diplôme";
+    } elseif ($moyenne < 12) {
+        $phrase = "Mention passable";
+    } elseif ($moyenne < 14) {
+        $phrase = "Mention assez bien";
+    } elseif ($moyenne < 16) {
+        $phrase = "Mention bien";
+    } elseif ($moyenne <= 20) {
+        $phrase = "Mention très bien";
+    } else {
+        $phrase = "Les notes sont mal saisies";
+    }
+    return $phrase;
+}
+
+$liste = [];
+$longueurListe = count($liste);
+
+if ($liste != []) {
+
+    $phrase = null;
+    $total = 0;
+
+    foreach ($liste as $note) {
+        $total += $note;
+    }
+
+    $moyenne = round($total / $longueurListe,2);
+
+    echo calculMention($moyenne);
+} else {
+    echo "Veuillez saisir des notes";
+}
+
