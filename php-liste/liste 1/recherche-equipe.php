d474636f6c594733daa06f861c1e2bb12a415be1
bassignotA <misterbalise2@gmail.com>
2022-10-19 15:11:34 +0200

    15:10

---
+++ php-liste/liste 1/recherche-equipe.php
@@ -0,0 +1,38 @@
+<?php
+
+function verifieSiEquipeBoucle($equipes, $choixUtilisateur) {
+    $choixUtilisateur = strtolower($choixUtilisateur);
+    $longueurEquipes = count($equipes);
+    if ($longueurEquipes > 0) {
+        foreach ($equipes as $equipe) {
+            if (strtolower($equipe) == $choixUtilisateur){
+                return true;
+            }
+        }
+        return false;
+    }
+    return false;
+}
+
+function afficheResultat($siChoixDansEquipe,$choixUtilisateur)
+{
+    if ($siChoixDansEquipe) {
+        echo "L'équipe de $choixUtilisateur participe à la prochaine coupe du monde !";
+    } else {
+        echo "L'équipe de $choixUtilisateur ne participe pas à la prochaine coupe du monde !";
+
+    }
+}
+$equipes = ['France','Brésil','Argentine','Espagne','Sénégal','Australie','Belgique'];
+
+$choixUtilisateur = ucfirst(strtolower(readline("Saisir une équipe : ")));
+
+echo "Version boucle : "."\n";
+$siChoixDansEquipe = verifieSiEquipeBoucle($equipes,$choixUtilisateur);
+afficheResultat($siChoixDansEquipe,$choixUtilisateur);
+
+echo PHP_EOL;
+
+echo "Version fonctions implementées : "."\n";
+$siChoixDansEquipe = in_array($choixUtilisateur,$equipes);
+afficheResultat($siChoixDansEquipe,$choixUtilisateur);
