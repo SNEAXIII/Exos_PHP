7bc74a60ca2725c232aab38f5efe9501791da4b4
bassignotA <misterbalise2@gmail.com>
2022-11-16 16:59:46 +0100

    16 11 2022 17h00

--- php-fonctions/tableau.php
+++ php-fonctions/tableau.php
@@ -1,1 +1,12 @@
 <?php
+$tab = [12,15,16,17,55,44];
+$tab = [12,15,"s",17,"ssss",44];
+
+function afficherTableau(array $tab) {
+    foreach ($tab as $elem) {
+        echo "$elem ";
+    }
+    echo "\n";
+}
+
+afficherTableau($tab);
