927f96800304679008b5b53f57cb13e75c661807
bassignotA <misterbalise2@gmail.com>
2022-11-30 14:25:59 +0100

    30 11 2022 14h25

--- php-database-version-etudiant/requetes/R6.php
+++ php-database-version-etudiant/requetes/R6.php
@@ -7,6 +7,21 @@ require "../base-de-donnees/tableCategorie.php";
 /* Requête 6
  * Récupérer le nombre d'articles postés par un auteur donné (id_auteur)
 */
+function r6(array $tableArticles) : int{
+    $auteurId = readline(3);
+    $nb = 0;
+    echo "test";
+    foreach ($tableArticles as $item) {
+        $siIdItemEgalIdUtilisateur = $auteurId == $item["id_auteur"];
+        if ($siIdItemEgalIdUtilisateur) $nb++;
+    }
+    return $nb;
 
-$auteurId = readline("Saisir l'id d'un auteur : ");
+}
 
+
+$resultat = r6($tableArticles);
+//foreach ($resultats as $item) {
+//    echo $item["id"]." ";
+//}
+echo $resultat;
--- php-database-version-etudiant/requetes/testr5.php
+++ php-database-version-etudiant/requetes/testr5.php
@@ -1,17 +1,25 @@
 <?php
 
-require "../base-de-donnees/tableArticle.php";
-require "../base-de-donnees/tableAuteur.php";
-require "../base-de-donnees/tableCategorie.php";
+//require "../base-de-donnees/tableArticle.php";
+//require "../base-de-donnees/tableAuteur.php";
+//require "../base-de-donnees/tableCategorie.php";
 
-foreach ($tableArticles as $item) {
-    echo $item["titre"]."\n";
+$tableAuteurs = [
+    "1" =>["prenom"=>"jean","nom"=>"dupond","age"=>60],
+    "2" =>["prenom"=>"alain","nom"=>"durand","age"=>18],
+    "3" =>["prenom"=>"anne","nom"=>"martin","age"=>84],
+    "4" =>["prenom"=>"marie","nom"=>"thomas","age"=>25],
+    "5" =>["prenom"=>"pierre","nom"=>"sapin","age"=>54],
+];
+
+foreach ($tableAuteurs as $item) {
+    echo $item["prenom"]."\n";
 }
 
-$collone = array_column($tableArticles, "titre");
-array_multisort($collone,SORT_ASC, SORT_STRING, $tableArticles);
+$collone = array_column($tableAuteurs, "age");
+array_multisort($collone,SORT_ASC, SORT_STRING, $tableAuteurs);
 
-echo "--------------------------------------------";
-foreach ($tableArticles as $item) {
-    echo $item["titre"]."\n";
+echo "--------------------------------------------"."\n";
+foreach ($tableAuteurs as $item) {
+    echo $item["prenom"]."\n";
 }
