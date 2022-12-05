e3898d03ef6ef13fc0e621d0dfddcbc0e2f5d8a2
bassignotA <misterbalise2@gmail.com>
2022-12-05 14:33:38 +0100

    05 12 2022 14h30

--- php-database-version-etudiant/requetes/R7.php
+++ php-database-version-etudiant/requetes/R7.php
@@ -8,16 +8,41 @@ require "../base-de-donnees/tableCategorie.php";
  * Récupérer le nombre d'articles postés par chaque auteur
  * On souhaite récupérer l'id, le prénom, le nom et le nombre d'articles
 */
-
-function r7() {
+/* // version avec double boucles
+function r7(array $tableArticles,$tableAuteurs) : array {
     $resultats = [];
-    $nombreArticleParAuteur = array_count_values()
-    foreach ($tableArticle as $id=>$article) {
-
+    $nombreArticleParAuteur = array_count_values(array_column($tableArticles, 'id_auteur'));
+    foreach ($tableArticles as $idArticle=>$article) {
+        foreach ($tableAuteurs as $idAuteur=>$auteur) {
+            if (! array_key_exists($idAuteur, $resultats) && $idArticle == $idAuteur) {
+                $resultats[$idAuteur] = [
+                    "prenom" => $auteur["prenom"],
+                    "nom" => $auteur["nom"],
+                    "nombreArticlesEcrits" => $nombreArticleParAuteur[$idAuteur]
+                ];
+            }
+        }
     }
+    return $resultats;
+}
+*/
+//version rapide
+function r7(array $tableArticles,$tableAuteurs) : array {
+    $resultats = [];
+    $nombreArticleParAuteur = array_count_values(array_column($tableArticles, 'id_auteur'));
+    foreach ($tableAuteurs as $idAuteur=>$auteur) {
+        $resultats[$idAuteur] = [
+            "prenom" => $auteur["prenom"],
+            "nom" => $auteur["nom"],
+            "nombreArticlesEcrits" => $nombreArticleParAuteur[$idAuteur]
+        ];
+    }
+    return $resultats;
 }
 
 
 // test
+
+$resultats = r7($tableArticles,$tableAuteurs);
 
 print_r($resultats);
