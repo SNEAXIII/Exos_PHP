ca8dd36d6467e9a54420f5a5136e5b907a93ae87
Alexis Bassignot <misterbalise2@gmail.com>
2023-01-18 16:15:57 +0100

    18/01/2023 16:15:57,52

--- update.bat
+++ update.bat
@@ -1,9 +1,15 @@
 @echo off
 echo Ajout des fichiers...
+echo __________________________
 git add -A
 echo Commit en cours... %date% %time%
+echo __________________________
 git commit -m "%date% %time%"
+echo __________________________
 echo Push en cours...
+echo __________________________
 git push -u origin main
+echo __________________________
 echo Fin du push
+echo __________________________
 pause
