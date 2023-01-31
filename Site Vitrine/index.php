0fed680b88c33b85c9c5a4f3032ddfdbd064d8ab
Alexis Bassignot <misterbalise2@gmail.com>
2023-01-31 11:55:27 +0100

    31/01/2023 11:55:26,93

---
+++ Site Vitrine/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang=fr>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styles.css">
+    <title>Page vitrine</title>
+</head>
+<body>
+<header>
+    <img id="logo" alt="" src="src/image/logo_wide.png">
+    <nav>
+        <ul>
+            <li>nos produits</li>
+            <li>A propos</li>
+            <li>historique</li>
+            <li>nos services</li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
