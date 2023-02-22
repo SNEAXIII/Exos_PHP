e4d66c0f02c7acb90d55126657648efbc71e2dc7
Alexis Bassignot <misterbalise2@gmail.com>
2023-02-22 17:39:19 +0100

    22/02/2023 17:39:18,98

--- tp-database-etudiant/index.php
+++ tp-database-etudiant/index.php
@@ -11,7 +11,7 @@ $students = selectAllStudent();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="styles.css">
-    <title>Des jolis étudiants</title>
+    <title>Best Students</title>
 </head>
 <body>
 <screen>
