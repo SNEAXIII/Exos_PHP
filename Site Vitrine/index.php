c4a574cd8463d57b6300872688d9341c5b5841b1
Alexis Bassignot <misterbalise2@gmail.com>
2023-02-22 10:03:47 +0100

    22/02/2023 10:03:46,68

--- Site Vitrine/index.php
+++ Site Vitrine/index.php
@@ -21,8 +21,20 @@
     </nav>
 </header>
 <section>
-    <img class="imagePrincipale" src="bulk_bank_image/realisation1.jpg" alt="">
-    <img class="imagePrincipale" src="bulk_bank_image/realisation2.jpg" alt="">
+    <h1 style="grid-area: titre1">Présentation</h1>
+    <p class="presentation" style="grid-area: presentation">
+        ÆRIS BTP est présente sur le secteur depuis 1984 afin de vous aider à concevoir votre projet, de l'idée
+        jusqu'à la réalisation finale. Nous intégrons une démarche éco-responsable à notre processus dans le but
+        d'optimiser les éclairages et le chauffage afin de réduire la consomation électrique. Vous trouverez ci-dessous
+        quelques réalisations.</p>
+    <div style="grid-area: fondateur"></div>
+    <h1 style="grid-area: titre2">Nos réalisations</h1>
+    <img class="imageIllustration" style="grid-area: imageIllustration1" src="bulk_bank_image/realisation2.jpg" alt="">
+    <span class="legende" style="grid-area: legende1">Nouvelle mairie de Meilleraie-Tillay<br>2018</span>
+    <img class="imageIllustration" style="grid-area: imageIllustration2" src="bulk_bank_image/realisation1.jpg" alt="">
+    <span class="legende" style="grid-area: legende2">Nouvelle mairie d'Echenoz-la-Méline<br>2012</span>
+
+<!--rerer-->
 </section>
 </body>
 </html>
