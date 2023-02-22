b3001ec0a90d5ef9fa8b5e21237e5cecd0e8b260
Alexis Bassignot <misterbalise2@gmail.com>
2023-02-22 13:50:38 +0100

    22/02/2023 13:50:37,88

--- tp-database-etudiant/src/utils/dates.php
+++ tp-database-etudiant/src/utils/dates.php
@@ -4,7 +4,7 @@ include_once 'src\modele\etudiantDB.php';
 function calculeAgeEtudiant(string $dateNaissance) : array
 {
     $age = date_diff(date_create($dateNaissance),date_create(date("Y-m-d")))->format('%y');
-    $couleur = $age<18?"#ff0000":"#33ff00";
+    $couleur = $age<18?"#ff0000":"#379A28FF";
     return ["age"=>$age,"couleur"=>$couleur];
 }
 function formateDate($dateNaissance) : string
