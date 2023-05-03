94fa3457db4a603c77601d27c4ff46058e08d2da
Alexis Bassignot <misterbalise2@gmail.com>
2023-05-03 17:49:42 +0200

    03/05/2023 17:49:41,20

---
+++ outils/auto_prettier.bat
@@ -0,0 +1,4 @@
+@echo off
+npm -y init
+npm install --save-dev --save-exact prettier
+npm install --save-dev --save-exact @prettier/plugin-php
---
+++ php-biblio/Auteur.php
@@ -0,0 +1,8 @@
+<?php
+require_once "Livre.php";
+class Auteur
+{
+    private string $nom;
+    private string $prenom;
+
+}
